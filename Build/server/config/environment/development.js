d57d2af7db1ade31610797c0d6a97c9984f74002
Jason Brannon <jason.brannon@inforeliance.com>
2015-06-19 16:55:44 -0400

    Baseline scaffolding

---
+++ Build/server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://127.0.0.1/medcheck-dev'
+  },
+
+  seedDB: true
+};
