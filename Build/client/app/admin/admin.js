8f3f65ef67fa2cb4231e9bbac92a4fb6d308af36
Jason Brannon <jason.brannon@inforeliance.com>
2015-06-19 16:49:57 -0400

    Refactored baseline for MedCheck

---
+++ Build/client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('medCheckApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
