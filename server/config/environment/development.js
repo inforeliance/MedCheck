9ad3b504d30024dfcb284dc83deddd717e84a108
Jason Brannon <campbellgolf@gmail.com>
2015-06-19 15:40:46 -0400

    Baseline scaffolding

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://127.0.0.1/dieless-dev'
+  },
+
+  seedDB: true
+};
