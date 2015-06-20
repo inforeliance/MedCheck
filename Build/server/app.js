0cdf1235e4cfa024a2918b16e2a12cb76f5e70e8
Jason Brannon <jason.brannon@inforeliance.com>
2015-06-20 05:50:33 -0400

    Added node.js type scripting for VS Code IDE

--- Build/server/app.js
+++ Build/server/app.js
@@ -1,3 +1,4 @@
+/// <reference path="../typings/node/node.d.ts"/>
 /**
  * Main application file
  */
@@ -9,7 +10,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
 var mongoose = require('mongoose');
-var config = require('./config/environment');
+var config = require('./config/environment')
+
 
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
