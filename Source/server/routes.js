235496862054ead2b65d7157b38ef3190366e026
Tim Clark <tim.clark@inforeliance.com>
2015-06-21 18:07:12 -0400

    added quaggajs

--- Source/server/routes.js
+++ Source/server/routes.js
@@ -8,19 +8,24 @@ var errors = require('./components/errors');
 
 module.exports = function(app) {
 
-  // Insert routes below
+  // Insert routes below  
   app.use('/api/things', require('./api/thing'));
-  app.use('/api/users', require('./api/user'));
+  app.use('/api/users', require('./api/user')); 
 
   app.use('/auth', require('./auth'));
   
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
+  
+    var express = require("express");
+  
+    app.use('/node_modules', express.static('node_modules'));
+
 
   // All other routes should redirect to the index.html
-  app.route('/*')
-    .get(function(req, res) {
-      res.sendfile(app.get('appPath') + '/index.html');
-    });
+   app.route('/*')
+     .get(function(req, res) {
+       res.sendfile(app.get('appPath') + '/index.html');
+     });
 };
