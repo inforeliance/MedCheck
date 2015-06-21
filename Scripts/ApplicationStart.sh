dfb001de51206a8a7a1a45d096a3cccdde97d709
irrossserino <ross.Serino@inforeliance.com>
2015-06-21 19:10:49 -0400

    update to application install

--- Scripts/ApplicationStart.sh
+++ Scripts/ApplicationStart.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-/var/www/MedCheck/Source/npm update
-/var/www/MedCheck/Source/npm install
-/var/www/MedCheck/Source/bower install
-/var/www/MedCheck/Source/grunt build
+cd /var/www/MedCheck/Source/
+npm update
+npm install
+bower install
+grunt build
