ae2930d64497944f7e348ebdcd1f15f8e9d701ad
Tim Clark <tim.clark@inforeliance.com>
2015-07-03 16:15:20 -0400

    added exit 0 to scripts

--- Scripts/AfterInstall.sh
+++ Scripts/AfterInstall.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 chown ubuntu /var/www/MedCheck/ -R 
 apt-get update
+exit 0
--- Scripts/ApplicationStart.sh
+++ Scripts/ApplicationStart.sh
@@ -9,3 +9,4 @@ grunt build
 cd /var/www/MedCheck/Source/dist/server
 export NODE_ENV=production
 nodejs app.js >> /home/ubuntu/gruntlog/nodelog.log &
+exit 0
